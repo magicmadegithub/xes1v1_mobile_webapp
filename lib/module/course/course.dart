05dc738eb519a01c57d6e356a9b29d7157d5a4f5
magic <mxinbeijing@foxmail.com>
2020-06-28 23:22:29 +0800

    完善课程页面

--- lib/module/course/course.dart
+++ lib/module/course/course.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:xes1v1mobileweb/common/text_styles.dart';
 import 'package:xes1v1mobileweb/net/models/course_list_entity.dart';
-import 'package:xes1v1mobileweb/net/models/frame_list_entity.dart';
 import 'package:xes1v1mobileweb/net/net_url.dart';
 import 'package:xes1v1mobileweb/net/net_utils.dart';
-
-import 'course_viewpage.dart';
 
 class Course extends StatefulWidget {
   @override
@@ -16,8 +13,29 @@ class Course extends StatefulWidget {
 }
 
 class CourseState extends State<Course> {
-  List<CourseEntity> _list;
-  List<String> _images = [];
+  List<CourseEntity> _list = [];
+  List<Widget> _images = [
+    Image.asset(
+      'images/it_word1.jpg',
+      fit: BoxFit.fill,
+    ),
+    Image.asset(
+      'images/it_word2.jpg',
+      fit: BoxFit.fill,
+    ),
+    Image.asset(
+      'images/it_word3.jpg',
+      fit: BoxFit.fill,
+    ),
+    Image.asset(
+      'images/it_word4.jpg',
+      fit: BoxFit.fill,
+    ),
+    Image.asset(
+      'images/it_word5.jpg',
+      fit: BoxFit.fill,
+    ),
+  ];
 
   @override
   void initState() {
@@ -26,10 +44,9 @@ class CourseState extends State<Course> {
       CourseListEntity result = CourseListEntity.fromJson(data);
       if (result.code == 0) {
         _list = result.data;
-        _list?.forEach((course) {
-          _images.add(course.imgurl);
+        setState(() {
+          
         });
-        setState(() {});
       }
     });
   }
@@ -40,57 +57,71 @@ class CourseState extends State<Course> {
       child: Column(
         children: [
           Container(
-            child: new Swiper(
+            child: Swiper(
               itemBuilder: (BuildContext context, int index) {
-                return new Image.network(
-                  _images[index],
-                  fit: BoxFit.fill,
-                );
+                return _images[index];
               },
               itemCount: _images.length,
-              pagination: new SwiperPagination(),
-              control: new SwiperControl(),
+              pagination: SwiperPagination(),
+              loop: true,
+              autoplay: true,
+              autoplayDelay: 4000,
+              viewportFraction: 0.7,
+              scale: 0.8,
             ),
             height: 200,
           ),
           Expanded(
             child: Container(
-              margin: EdgeInsets.only(top: 20),
               child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  mainAxisSpacing: 5,
-                  crossAxisSpacing: 10,
-                  childAspectRatio: 1.15,
-                ),
-                itemBuilder: (context, index) {
-                  CourseEntity course = _list[index];
-                  return Container(
-                    child: Card(
-                      child: Column(
-                        children: [
-                          Container(
-                            child: ClipRRect(
-                                child: Image.network(course.imgurl),
-                                borderRadius: BorderRadius.circular(10)),
-                            margin: EdgeInsets.all(10),
-                          ),
-                          Container(
-                            margin: EdgeInsets.all(10),
-                            child: Text(
-                              course.title,
-                              style: TextStyles.course,
-                            ),
-                            alignment: Alignment.center,
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-                itemCount: _list.length,
-              ),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 5,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemCount: _list.length,
+                  itemBuilder: (context, index) {
+                    CourseEntity course = _list[index];
+                    return Container(color: Colors.red,);
+                  }),
             ),
+//            child: Container(
+//              margin: EdgeInsets.only(top: 20),
+//              child: GridView.builder(
+//                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                  crossAxisCount: 3,
+//                  mainAxisSpacing: 5,
+//                  crossAxisSpacing: 10,
+//
+//                ),
+//                itemBuilder: (context, index) {
+//                  CourseEntity course = _list[index];
+//                  return Container(
+//                    child: Card(
+//                      child: Column(
+//                        children: [
+//                          Container(
+//                            child: ClipRRect(
+//                                child: Image.network(course.imgurl),
+//                                borderRadius: BorderRadius.circular(10)),
+//                            margin: EdgeInsets.all(10),
+//                          ),
+//                          Container(
+//                            margin: EdgeInsets.all(10),
+//                            child: Text(
+//                              course.title,
+//                              style: TextStyles.course,
+//                            ),
+//                            alignment: Alignment.center,
+//                          ),
+//                        ],
+//                      ),
+//                    ),
+//                  );
+//                },
+//                itemCount: _list.length,
+//              ),
+//            ),
           ),
         ],
       ),
