9bc86667090ced1627544e7ffa7d658e49522c86
peter <maxu@100tal.com>
2020-06-29 21:08:19 +0800

    增加荣誉页面

--- lib/module/honor/honor.dart
+++ lib/module/honor/honor.dart
@@ -1,10 +1,33 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:xes1v1mobileweb/module/honor/honor_trophy.dart';
 
 class Honor extends StatelessWidget {
+  final sides = [60.0, 50.0, 40.0, 30.0];
+  final colors = [Colors.red, Colors.yellow, Colors.blue, Colors.green];
+
+  Widget _buildItem(double e) {
+    return Container(
+      width: e,
+      alignment: Alignment.center,
+      height: e,
+      color: colors[sides.indexOf(e)],
+      child: Text('$e'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.teal,
+      child: HonorTrophyWidget(
+        children: sides.map((e) => _buildItem(e)).toList(),
+        menu: Image.asset(
+          'images/honor_trophy.png',
+          width: 400,
+          height: 400,
+        ),
+      ),
     );
   }
 }
---
+++ lib/module/honor/honor_trophy.dart
@@ -0,0 +1,90 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class HonorTrophyWidget extends StatefulWidget {
+  final List<Widget> children;
+  final Widget menu;
+
+  HonorTrophyWidget({@required this.children, @required this.menu});
+
+  @override
+  _HonorTrophyState createState() => _HonorTrophyState();
+}
+
+class _HonorTrophyState extends State<HonorTrophyWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  double _rad = 0.0;
+  bool _closed = true;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+        duration: Duration(milliseconds: 1000), vsync: this)
+      ..addListener(() => setState(
+          () => _rad = (_closed ? (_controller.value) : 1 - _controller.value)))
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _closed = !_closed;
+        }
+      });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Flow(
+      delegate: _CircleFlowDelegate(_rad),
+      children: [
+        ...widget.children,
+        InkWell(
+            onTap: () {
+              _controller.reset();
+              _controller.forward();
+            },
+            child: widget.menu)
+      ],
+    );
+  }
+}
+
+class _CircleFlowDelegate extends FlowDelegate {
+  final double rad;
+
+  _CircleFlowDelegate(this.rad);
+
+  @override //绘制孩子的方法
+  void paintChildren(FlowPaintingContext context) {
+    double radius = context.size.shortestSide / 2;
+    var count = context.childCount - 1;
+    var perRad = 2 * pi / count;
+    for (int i = 0; i < count; i++) {
+      print(i);
+      var cSizeX = context.getChildSize(i).width / 2;
+      var cSizeY = context.getChildSize(i).height / 2;
+      var offsetX = rad * (radius - cSizeX) * cos(i * perRad) + radius;
+      var offsetY = rad * (radius - cSizeY) * sin(i * perRad) + radius;
+      context.paintChild(i,
+          transform: Matrix4.translationValues(
+              offsetX - cSizeX, offsetY - cSizeY, 0.0));
+    }
+    context.paintChild(context.childCount - 1,
+        transform: Matrix4.translationValues(
+            radius - context.getChildSize(context.childCount - 1).width / 2,
+            radius - context.getChildSize(context.childCount - 1).height / 2,
+            0.0));
+  }
+
+  @override
+  bool shouldRepaint(FlowDelegate oldDelegate) {
+    return true;
+  }
+}
